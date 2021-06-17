1c7673929e362f41af1290b6de61c364ccbc1afd
Shahram Talei <stalei@crimson.ua.edu>
2021-06-17 14:06:47 -0500

    BE sort

--- TagMix.py
+++ TagMix.py
@@ -144,7 +144,7 @@ if __name__ == "__main__":
             #BE.sort(key=lambda x: x[0],reverse=True)
             BE.sort(reverse=True)
             #print(BE.shape)
-            print(np.array(BE).shape)
+            print(BE)
             #quicksort(BE2)
             #BErev=BE2[::-1] #reverse it
             #print(BE)
